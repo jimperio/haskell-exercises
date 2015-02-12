ad989109deaf31559283f0925ce1889dfb571447
Jomel Imperio <jimperio@gmail.com>
2015-02-12 18:22:28 +0800

    Nicer drop with pattern-matching and guards

--- rwh/ch2/myDrop.hs
+++ rwh/ch2/myDrop.hs
@@ -3,3 +3,8 @@ myDrop :: Int -> [a] -> [a]
 myDrop n xs = if n <= 0 || null xs
               then xs
               else myDrop (n - 1) (tail xs)
+
+-- we can use guards this way to make it cleaner!
+niceDrop n xs | n <= 0 = xs
+niceDrop _ []          = []
+niceDrop n (_:xs)      = niceDrop (n - 1) xs
