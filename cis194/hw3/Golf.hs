d00863351efba718f780eb2f33f1f958c7366cd6
Jomel Imperio <jimperio@gmail.com>
2015-10-06 13:54:28 +0800

    Add histogram solution

--- cis194/hw3/Golf.hs
+++ cis194/hw3/Golf.hs
@@ -1,3 +1,8 @@
+import Data.Array (Array)
+import qualified Data.Array as Array
+import Data.Char
+import Data.List
+
 -- Exercise 1: Hopscotch
 skips :: [a] -> [[a]]
 skips xs = map (every xs) [1..length xs]
@@ -43,5 +48,29 @@ hasMax _            = False
 getMax :: [Integer] -> Integer
 getMax (_:y:_:[]) = y
 
+
 -- Exercise 3: Histogram
--- histogram :: [Integer] -> String
+-- * Start with an array containing strings of the format
+--   "0=", "1=", .., "9=".
+-- * Consume the list, adding '*' to the corresponding string
+--   and keeping track of the maximum length.
+-- * Convert from horizontal to vertical histogram by:
+--   * Padding the strings with spaces to the maximum length.
+--   * Transpose, reverse, then join the strings.
+histogram :: [Integer] -> String
+histogram is = unlines $ reverse $ transpose (f n 0 is)
+  where n    = Array.array (0, 9) [(i, (intToDigit i) : "=") | i <- [0..9]]
+
+-- This doesn't handle unexpected inputs, e.g., integers not in [0..9].
+f :: Array Int String -> Int -> [Integer] -> [String]
+f cs m (i:is) = f cs' m' is
+  where cs'   = cs Array.// [(j, cs Array.! j ++ "*")]
+        j     = fromIntegral i
+        m'    = max m n
+        n     = length (cs' Array.! j)
+f cs m []     = map (p m) (Array.elems cs)
+
+p :: Int -> String -> String
+p m s
+  | length s < m = p m (s ++ " ")
+  | otherwise    = s
